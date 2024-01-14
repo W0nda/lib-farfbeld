75430335b5277007a4319abaabe8bb9d288a024e
W0nda <luclemauff@outlook.fr>
2024-01-14 16:56:28 +0000

    Resolve ffstruct2ff() endianness problem

--- ffstruct2ff.c
+++ ffstruct2ff.c
@@ -4,12 +4,23 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+int reverseEndianness(int val)
+{
+    return (val << 24) |
+          ((val <<  8) & 0x00ff0000) |
+          ((val >>  8) & 0x0000ff00) |
+          ((val >> 24) & 0x000000ff);
+}
+
 void ffstruct2ff(ffstruct *ffstruct, char *filename){
 	int foutput = open(filename, O_WRONLY);
 
 	write(foutput,ffstruct->magic_value,sizeof(ffstruct->magic_value));
-	write(foutput,&ffstruct->width,sizeof(ffstruct->width));
-	write(foutput,&ffstruct->height,sizeof(ffstruct->height));
+
+	int width = reverseEndianness(ffstruct->width);
+	int height = reverseEndianness(ffstruct->height);
+	write(foutput,&width,sizeof(width));
+	write(foutput,&height,sizeof(height));
 
 	int px_fieldsize = ffstruct->width * ffstruct->height * 8;
 	write(foutput,ffstruct->pixels,px_fieldsize);
